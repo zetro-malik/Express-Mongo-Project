97656392b9fc64dc94f5b081375bba7a5c6bf774
zetro-malik <zeeshanimsb@gmail.com>
2023-09-17 06:26:04 +0500

    add constants.js

--- mycontacts-backend/middleware/errorHandler.js
+++ mycontacts-backend/middleware/errorHandler.js
@@ -1,9 +1,15 @@
 
 // this middleware will work for error
 const errorHandler = (err,req,res,next)=>{
+ 
+ const statusCode =  res.statusCode ? res.statusCode : 500;
+ switch(statusCode){
+    case 400:
+ res.json({title:"Validation Failed", message:err.message,stackTrace:err.stack})
 
- console.log(err);
-
+        break;
+ }
+ res.json({title:"Not Found", message:err.message,stackTrace:err.stack})
  next();
 }
 
