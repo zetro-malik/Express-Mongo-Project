a5838c235a5bee143b108744f6d30913a64eea92
zetro-malik <zeeshanimsb@gmail.com>
2023-09-17 05:39:36 +0500

    basic CRUD implemeneted

--- mycontacts-backend/routes/contactRoutes.js
+++ mycontacts-backend/routes/contactRoutes.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.status(200).json({"message":"OK"})
+    res.status(200).json({"message":"Get all contacts"})
+})
+
+router.post('/',(req,res)=>{
+    res.status(200).json({"message":"Created Contact"})
+})
+
+
+router.put('/:id',(req,res)=>{
+    res.status(200).json({"message":`Update contact for ${req.params.id}`})
+})
+
+
+router.delete('/:id',(req,res)=>{
+    res.status(200).json({"message":`Delete contact for ${req.params.id}`})
 })
 
 
